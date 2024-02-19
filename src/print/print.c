603c63d200ee0babd51eb3e6c867faaaa321e2c9
Jip van Sommeren <jip.van.sommeren@gmail.com>
2024-02-19 09:23:57 +0100

    basic AST implemented

--- src/print/print.c
+++ src/print/print.c
@@ -15,19 +15,19 @@
 /**
  * @fn PRTprogram
  */
-node_st *PRTprogram(node_st *node)
-{
-  TRAVstmts(node);
-  printf("Arithmetic Operators Count:\n");
-  printf("+ : %d\n", PROGRAM_PLUSCOUNT(node));
-  printf("- : %d\n", PROGRAM_MINUSCOUNT(node));
-  printf("* : %d\n", PROGRAM_MULCOUNT(node));
-  printf("/ : %d\n", PROGRAM_DIVCOUNT(node));
-  printf("%% : %d\n", PROGRAM_MODCOUNT(node));
+// node_st *PRTprogram(node_st *node)
+// {
+//   TRAVs(node);
+//   printf("Arithmetic Operators Count:\n");
+//   printf("+ : %d\n", PROGRAM_PLUSCOUNT(node));
+//   printf("- : %d\n", PROGRAM_MINUSCOUNT(node));
+//   printf("* : %d\n", PROGRAM_MULCOUNT(node));
+//   printf("/ : %d\n", PROGRAM_DIVCOUNT(node));
+//   printf("%% : %d\n", PROGRAM_MODCOUNT(node));
 
-  TRAVchildren(node);
-  return node;
-}
+//   TRAVchildren(node);
+//   return node;
+// }
 
 /**
  * @fn PRTstmts
@@ -164,5 +164,141 @@ node_st *PRTbool(node_st *node)
 {
   char *bool_str = BOOL_VAL(node) ? "true" : "false";
   printf("%s", bool_str);
+  return node;
+}
+
+/**
+ * @fn PRTifstmt
+ */
+node_st *PRTif(node_st *node)
+{
+  printf("if (");
+  TRAVopt(IF_CONDITION(node));
+  printf(") ");
+  TRAVdo(IF_THENBRANCH(node));
+  if (IF_ELSEBRANCH(node) != NULL)
+  {
+    printf(" else ");
+    TRAVdo(IF_ELSEBRANCH(node));
+  }
+  return node;
+}
+
+/**
+ * @fn PRTwhile
+ */
+node_st *PRTwhile(node_st *node)
+{
+  printf("while (");
+  TRAVopt(WHILE_CONDITION(node));
+  printf(") ");
+  TRAVdo(WHILE_BODY(node));
+  return node;
+}
+
+/**
+ * @fn PRTfor
+ */
+node_st *PRTfor(node_st *node)
+{
+  printf("for (");
+  TRAVopt(FOR_INIT(node));
+  printf("; ");
+  TRAVopt(FOR_CONDITION(node));
+  printf("; ");
+  TRAVopt(FOR_UPDATE(node));
+  printf(") ");
+  TRAVdo(FOR_BODY(node));
+  return node;
+}
+
+/**
+ * @fn PRTreturn
+ */
+node_st *PRTreturn(node_st *node)
+{
+  printf("return ");
+  TRAVopt(RETURN_RETURNVALUE(node));
+  printf(";\n");
+  return node;
+}
+
+/**
+ * @fn PRTblock
+ */
+node_st *PRTblock(node_st *node)
+{
+  printf("{\n");
+  TRAVopt(BLOCK_STATEMENTS(node));
+  printf("}\n");
+  return node;
+}
+
+/**
+ * @fn PRTmonop
+ */
+node_st *PRTmonop(node_st *node)
+{
+  switch (MONOP_OP(node))
+  {
+  case MO_not:
+    printf("!");
+    break;
+  case MO_neg:
+    printf("-");
+    break;
+  }
+  TRAVopt(MONOP_OPERAND(node));
+  return node;
+}
+
+/**
+ * @fn PRTconst
+ */
+node_st *PRTconst(node_st *node)
+{
+  // Assuming CONST_VALUE is a macro to access the value of the Const node
+  printf("%s", CONST_VALUE(node));
+  return node;
+}
+
+/**
+ * @fn PRTfuncallexpr
+ */
+node_st *PRTfuncallexpr(node_st *node)
+{
+  printf("%s(", FUNCALLEXPR_FUNCTIONNAME(node));
+  TRAVopt(FUNCALLEXPR_ARGUMENTS(node));
+  printf(")");
+  return node;
+}
+
+/**
+ * @fn PRTprogram
+ */
+node_st *PRTprogram(node_st *node)
+{
+  // Print the statements within the program
+  TRAVopt(PROGRAM_STMTS(node));
+
+  // Continue traversal for any other children of the Program node, if necessary
+  TRAVchildren(node);
+  return node;
+}
+
+/**
+ * @fn PRTexprlist
+ */
+node_st *PRTexprlist(node_st *node)
+{
+  // Print the first expression in the list
+  TRAVopt(EXPRLIST_EXPRESSION(node));
+
+  // If there is another expression in the list, print a comma and traverse it
+  if (EXPRLIST_NEXT(node) != NULL)
+  {
+    printf(", ");
+    TRAVdo(EXPRLIST_NEXT(node));
+  }
   return node;
 }
